eec03e34a62fc622a2bea04c1aeac457752eb5b2
Mallinath Nagaraj <mallinathin@gmail.com>
2017-06-04 14:47:12 -0700

    add entities in the models directory

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace OnlineShop
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Tour> Tours { get; set; }
+    }
+}
