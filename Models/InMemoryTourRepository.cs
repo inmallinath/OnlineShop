1c8b9a7e7e3bb73d83341814046448a097ff914e
Mallinath Nagaraj <mallinathin@gmail.com>
2017-06-05 20:09:22 -0700

    update image files and clean up code

--- Models/InMemoryTourRepository.cs
+++ Models/InMemoryTourRepository.cs
@@ -14,30 +14,30 @@ namespace OnlineShop
         }
 
         public IEnumerable<Tour> Tours => new List<Tour>{
-            new Tour{ TourId = 1, Name = "Canadian F1 Grand Prix", ShortDescription = "F1 Event at Montreal", 
+            new Tour{ TourId = 1, Name = "Canadian F1 GP", ShortDescription = "F1 Event at Montreal", 
                         LongDescription = "Occurs once in a Calendar Year and held at the vibrant city of Montreal. This Canadian event will bring all canadian and north american motor sports enthusiasts together. The tour price includes event tickets for all three days - Warm Up, Qualification and Race Day", 
-                        Price = 1250.65M, Category = _categoryRepository.Categories.ToList()[0],
-                        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Canadian_GP_start_2008.jpg/1024px-Canadian_GP_start_2008.jpg", 
+                        Price = 1250.65M, Category = _categoryRepository.Categories.ToList()[0], 
+                        ImageUrl="http://i65.tinypic.com/2ajtg8y.jpg",
                         SeatsAvailable = false, IsTourOfTheWeek = false,
-                        ImageThumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/2012_Canadian_Grand_Prix_Fernando_Alonso_Ferrari_F2012-02.jpg/1280px-2012_Canadian_Grand_Prix_Fernando_Alonso_Ferrari_F2012-02.jpg"},
+                        ImageThumbnailUrl="http://i67.tinypic.com/flvjwn.jpg"},
             new Tour{TourId = 2, Name = "Alaskan Wilderness", ShortDescription = "Get Adventurous at Alaska",
                         LongDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer finibus luctus turpis, nec fermentum ante. Sed iaculis tempus mauris, et varius ante molestie vitae. Aliquam lobortis quam eu risus venenatis, vitae vehicula dolor varius. Suspendisse potenti.",
                         Price = 2230.15M, Category = _categoryRepository.Categories.ToList()[1],
-                        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/AK_koppen.svg/931px-AK_koppen.svg.png",
+                        ImageUrl="http://i65.tinypic.com/25jb7sm.png",
                         SeatsAvailable = true, IsTourOfTheWeek = true,
-                        ImageThumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/AK_koppen.svg/931px-AK_koppen.svg.png"},
-            new Tour{TourId = 3, Name = "NickelBack Canadian Tour", ShortDescription = "Follow your favorite rock band with this tour package",
+                        ImageThumbnailUrl="http://i66.tinypic.com/sphzx4.png"},
+            new Tour{TourId = 3, Name = "NickelBack Tour", ShortDescription = "Follow your favorite rock band with this tour package",
                         LongDescription = "Ut magna nunc, rutrum et leo non, commodo scelerisque neque. Nulla et purus eu tellus iaculis placerat. Nulla eleifend gravida nisi non aliquet. Morbi sit amet quam egestas, efficitur erat eget, bibendum turpis. Aliquam erat volutpat.",
                         Price = 3275.80M, Category = _categoryRepository.Categories.ToList()[2],
-                        ImageUrl="https://upload.wikimedia.org/wikipedia/commons/c/ca/Nickelback_%40_Perth_Arena_%2817_11_2012%29_%288261243464%29.jpg",
+                        ImageUrl="http://i66.tinypic.com/33dzia9.jpg",
                         SeatsAvailable = true, IsTourOfTheWeek = false,
-                        ImageThumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/c/ca/Nickelback_%40_Perth_Arena_%2817_11_2012%29_%288261243464%29.jpg"},
-            new Tour{TourId = 4, Name = "Hare Rama Hare Krishna", ShortDescription = "Seek spirituality with a visit to the societies of Hare Rama and Hare Krishna in North America",
+                        ImageThumbnailUrl="http://i68.tinypic.com/2cdtjmf.jpg"},
+            new Tour{TourId = 4, Name = "Seeking God", ShortDescription = "Seeking God - Spirituality",
                         LongDescription = "Ut magna nunc, rutrum et leo non, commodo scelerisque neque. Nulla et purus eu tellus iaculis placerat. Nulla eleifend gravida nisi non aliquet. Morbi sit amet quam egestas, efficitur erat eget, bibendum turpis. Aliquam erat volutpat.",
                         Price = 1875.35M, Category = _categoryRepository.Categories.ToList()[3],
-                        ImageUrl="https://upload.wikimedia.org/wikipedia/commons/4/44/Hare_Krishna_in_Helsinki_C_IMG_8105.JPG",
+                        ImageUrl="http://i67.tinypic.com/10rtjrp.jpg",
                         SeatsAvailable = true, IsTourOfTheWeek = false,
-                        ImageThumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/4/44/Hare_Krishna_in_Helsinki_C_IMG_8105.JPG"}
+                        ImageThumbnailUrl="http://i68.tinypic.com/o8yr92.jpg"}
         };
 
         public IEnumerable<Tour> TourOfTheWeek => throw new NotImplementedException();
