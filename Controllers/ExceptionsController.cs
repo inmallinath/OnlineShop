b54cfef9c3b4bee7c578bba2313c5b7e36124cf7
Mallinath Nagaraj <mallinathin@gmail.com>
2017-06-10 08:26:14 -0700

    add exception page to the site

---
+++ Controllers/ExceptionsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class ExceptionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
