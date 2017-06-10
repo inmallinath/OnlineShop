3b2c371f9836d0d864fc09ea2dd21aa3ef25adea
Mallinath Nagaraj <mallinathin@gmail.com>
2017-06-09 19:06:37 -0700

    create the account controller and the associated login view model

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
